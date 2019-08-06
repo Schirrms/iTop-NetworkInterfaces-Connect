43a42296e6b5e14bb49cade7d1cf2efb154d4d5b
Schirrms <schirrms@schirrms.net>
2019-08-06 18:04:48 +0200

    First Upload on GitHub of a former work (version 1.2.1)

---
+++ schirrms-networkinterfaces-connect/en.dict.schirrms-networkinterfaces-connect.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:lnkConnectableCIToNetworkDevice/Attribute:network_port_id' => 'Port on the Network Device',
+	'Class:lnkConnectableCIToNetworkDevice/Attribute:device_port_id' => 'Port on the edge Device',
+	'Class:PhysicalInterface/Attribute:connected_to_id' => 'Connected to remote port',
+));
+?>
---
+++ schirrms-networkinterfaces-connect/fr.dict.schirrms-networkinterfaces-connect.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:lnkConnectableCIToNetworkDevice/Attribute:network_port_id' => 'Port sur l\'équipement réseau',
+	'Class:lnkConnectableCIToNetworkDevice/Attribute:device_port_id' => 'Port sur l\'équipement d\'extrémité',
+	'Class:PhysicalInterface/Attribute:connected_to_id' => 'Connecté au port distant',
+));
+?>
